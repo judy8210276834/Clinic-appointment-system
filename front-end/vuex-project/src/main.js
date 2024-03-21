8c58d8a0ee5f39cefa11ed4bdec36caee058255c
judychang <judy8210276834@yahoo.com.tw>
2024-03-21 22:19:21 +0800

    安裝fontawesome套件、新增css

--- front-end/vuex-project/src/main.js
+++ front-end/vuex-project/src/main.js
@@ -1,6 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+library.add(fas);
+
+createApp(App)
+  .component('fa', FontAwesomeIcon)
+  .use(store)
+  .use(router)
+  .mount("#app");
